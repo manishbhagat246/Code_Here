02c99b4b238450e01c13cf0ef626f7ebee386231
Saurav Yadav <93671987+Saurav9895@users.noreply.github.com>
2024-03-04 13:26:46 +0530

    some changes done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import Head  from 'next/head'
-import { ToastContainer } from 'react-toastify'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { RecoilRoot } from "recoil";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { RecoilRoot } from 'recoil'
-
 export default function App({ Component, pageProps }: AppProps) {
-  return(
-    <RecoilRoot>
-     <Head>
-        <title>Binary's Code</title>
-        <meta name='description' content='Web application that contains leetcode problems amd vedio sollutions' />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel='icon' href='/top-logo.png'/>
-      </Head>
-      <ToastContainer/>
-      <Component {...pageProps} />
-    </RecoilRoot>
-  );
+	return (
+		<RecoilRoot>
+			<Head>
+				<title>LeetClone</title>
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<link rel='icon' href='/favicon.png' />
+				<meta
+					name='description'
+					content='Web application that contains leetcode problems and video solutions'
+				/>
+			</Head>
+			<ToastContainer />
+			<Component {...pageProps} />
+		</RecoilRoot>
+	);
 }
