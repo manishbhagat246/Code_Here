9d2209e272f1dbebb2837747c14cdd6c022d1d19
Saurav Yadav <93671987+Saurav9895@users.noreply.github.com>
2024-03-04 17:31:51 +0530

    Made some changes in name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<RecoilRoot>
 			<Head>
-				<title>Binary's Code</title>
+				<title>Binary&apos;s Code</title>
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/top-logo.png' />
 				<meta
